3f2a2eaa6406790e8e699f3c35d16e9ca809a908
Cuong Do <cuongdo926@gmail.com>
2024-12-05 09:38:01 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { faker } from '@faker-js/faker';
+
+import { FilterType, Todo } from './model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  items: Todo[] = [];
+  filterType: FilterType = 'all';
+
+  get activeCount() {
+    return this.items.filter((x) => !x.completed).length;
+  }
+
+  get filteredItems() {
+    switch (this.filterType) {
+      case 'active':
+        return this.items.filter((x) => !x.completed);
+      case 'completed':
+        return this.items.filter((x) => x.completed);
+      default:
+        return this.items;
+    }
+  }
+
+  ngOnInit(): void {
+    for (let index = 0; index < 5; index++) {
+      this.items.push({
+        name: faker.book.title(),
+        completed: faker.datatype.boolean(),
+      });
+    }
+  }
+
+  toggle(item: Todo) {
+    item.completed = !item.completed;
+  }
+
+  addTodo(input: HTMLInputElement) {
+    if (input.value) {
+      this.items.push({
+        name: input.value,
+        completed: false,
+      });
+    }
+    input.value = '';
+  }
+
+  setFilter(type: FilterType) {
+    this.filterType = type;
+  }
+
+  clear() {
+    this.items = this.items.filter((x) => !x.completed);
+  }
 }
